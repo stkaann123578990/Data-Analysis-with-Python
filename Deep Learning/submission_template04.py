0c367cc8d40c52a3a65616f528eb6955698eec04
stkaann123578990 <115802852+stkaann123578990@users.noreply.github.com>
2023-04-02 20:53:38 +0300

    Update submission_template04.py

--- Deep Learning/submission_template04.py
+++ Deep Learning/submission_template04.py
@@ -1,15 +1,24 @@
-import numpy as np
-import torch
-from torch import nn
-from torch.nn import functional as F
-
 class ConvNet(nn.Module):
     def __init__(self):
-        ...
+        super().__init__()
+        
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=3, kernel_size=5)
+        self.pool1 = nn.MaxPool2d(kernel_size=2)
+        self.conv2 = nn.Conv2d(in_channels=3, out_channels=5, kernel_size=3)
+        self.pool2 = nn.MaxPool2d(kernel_size=2)
+        self.flatten = nn.Flatten()
+        self.fc1 = nn.Linear(in_features=245, out_features=100)
+        self.fc2 = nn.Linear(in_features=100, out_features=10)
 
     
     def forward(self, x):
-        ...
-
-def create_model():
-    return ConvNet()
+        
+        x = F.relu(self.conv1(x))
+        x = self.pool1(x)
+        x = F.relu(self.conv2(x))
+        x = self.pool2(x)
+        x = self.flatten(x)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+   
